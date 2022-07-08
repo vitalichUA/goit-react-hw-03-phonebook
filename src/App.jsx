b9207d46b346c0a1fab763b89d79e0042adcf24f
Troian Yevhenii <vitalich.ua@gmail.com>
2022-07-08 23:23:05 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,30 @@ import { ContactsList } from './components/ContactsList/ContactsList';
 
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+    contacts: [],
     filter: '',
   }
+
+  componentDidMount() {
+    const cont = localStorage.getItem('contacts');
+    const contParse = JSON.parse(cont);
+
+    if (contParse) {
+      this.setState({contacts: contParse})
+    }
+ }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextCont = this.state.contacts;
+    const pervCont = prevState.contacts;
+
+    if (nextCont !== pervCont) {
+      localStorage.setItem('contacts', JSON.stringify(nextCont))
+    }
+  }
+  
+  
 
   validateContact = (data) => {
       const normalizedValue = data.name.toLowerCase();
